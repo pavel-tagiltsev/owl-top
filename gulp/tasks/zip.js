72ee2185acf580bb6871d94bda88e484cd71e5ee
Павел <pavel.tagiltsef@gmail.com>
2022-04-10 23:06:01 +0300

    Setting a task for creating ZIP

---
+++ gulp/tasks/zip.js
@@ -0,0 +1,20 @@
+import del from 'del'
+import zipPlugin from 'gulp-zip'
+
+export default () => {
+  const { src, dest } = app.gulp
+  const { distFolder, rootFolder } = app.path
+  const { plumber, notify } = app.plugins
+
+  del(`./${rootFolder}.zip`)
+
+  return src(`${distFolder}/**/*.*`)
+    .pipe(plumber(
+      notify.onError({
+        title: 'ZIP',
+        message: 'Error: <%= error.message %>'
+      })
+    ))
+    .pipe(zipPlugin(`${rootFolder}.zip`))
+    .pipe(dest('./'))
+}
--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ import reset from './gulp/tasks/reset.js'
 import images from './gulp/tasks/images.js'
 import js from './gulp/tasks/js.js'
 import files from './gulp/tasks/files.js'
+import zip from './gulp/tasks/zip.js'
 import {otfToTtf, ttfToWoff, transferWoff, fontStyle } from './gulp/tasks/fonts.js'
 
 function watcher() {
@@ -29,9 +30,13 @@ function watcher() {
 
 const fonts = gulp.series(otfToTtf, ttfToWoff, transferWoff, fontStyle)
 const mainTasks = gulp.series(fonts, gulp.parallel(pug, scss, js, images, files))
+
+const deployZIP = gulp.series(reset, mainTasks, zip)
+
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
 const build = gulp.series(reset, mainTasks)
 
 export { build }
+export { deployZIP }
 
 gulp.task('default', dev)

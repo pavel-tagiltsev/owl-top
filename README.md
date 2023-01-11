f866d9a2f70c2acdf40506319fa86e724961e662
Pavel Tagiltsev <pavel.tagiltsef@gmail.com>
2023-01-11 20:55:16 +0700

    Updating configuration for the package gulp-load-plugins

--- README.md
+++
@@ -1,6 +0,0 @@
-## Fix some bugs
-
-### "gulp-load-plugins"
-
-In order to rid off a bug with "gulp-load-plugins",
-need to follow this https://github.com/jackfranklin/gulp-load-plugins/issues/141
--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,13 +1,7 @@
-import path from 'path'
-import __dirname from '../../root.js'
 import gulpLoadPlugins from 'gulp-load-plugins'
 
 export default gulpLoadPlugins({
-  // Explanation of this code,
-  // look for in readme.md file in bug's section
-  // * start *
-  config: path.resolve(__dirname, 'package.json'),
-  // * end *
+  config: process.env.npm_package_json,
   pattern: ['gulp-*', 'gulp.*', '@*/gulp{-,.}*', 'browser-*'],
   rename: {
     'gulp-if': 'gulpIf'
